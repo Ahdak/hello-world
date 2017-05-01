f3c64962879fc4115ca8e5699e2061a74a55b16b
Ahdak <dammak.ahmedd@gmail.com>
2017-05-01 22:42:48 +0200

    Init WS

---
+++ hello-world/src/main/java/net/dammak/main/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package net.dammak.main;
+
+/**
+ * @author ahmeddammak
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
